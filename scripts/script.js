0c9fbde08ef2a19dee3994d53534db332f981b94
jayanthvarma134 <jayanthvarma134@gmail.com>
2020-07-05 01:39:50 +0530

    update #4
    new buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,18 @@
+//<this wont work>
+// let Location = document.getElementById("location");
+
+
 // let items= Array('Bengaluru', 'Chennai', 'Mumbai', 'Delhi', 'Hyderabad', 'Shimla')
 let items= Array('Bengaluru', 'Chennai');
 var item = items[Math.floor(Math.random() * items.length)];
+var celsius;
+var fahrenheit;
 
 window.addEventListener('load', ()=> {
 	const api = 'https://api.weatherapi.com/v1/current.json?key=22c08d9dbf8f458db3753108200407&q='+item;
 	const data =get_data(api);
 	data_then= data.then(res=>{
+		//Location.innerHTMl =res.location.name; wont work
 		document.getElementById("location").innerHTML = res.location.name+', '+res.location.region+', '+res.location.country;
 		document.querySelector(".temp_degree").innerHTML = res.current.temp_c;
 		document.getElementById("weather_description").innerHTML = res.current.condition.text;
@@ -13,6 +20,9 @@ window.addEventListener('load', ()=> {
 		document.getElementById("precipitation").innerHTML= res.current.precip_mm+'mm';
 		document.getElementById("humidity").innerHTML = res.current.humidity;
 		document.getElementById("temp_logo").src = 'https:'+res.current.condition.icon;
+
+		celsius=    res.current.temp_c;
+		fahrenheit= res.current.temp_f;
 	});
 });
 
@@ -34,6 +44,9 @@ document.getElementById("map_marker_button").onclick =function(){
 				document.getElementById("precipitation").innerHTML= res.current.precip_mm+'mm';
 				document.getElementById("humidity").innerHTML = res.current.humidity;
 				document.getElementById("temp_logo").src = 'https:'+res.current.condition.icon;
+
+				celsius=    res.current.temp_c;
+				fahrenheit= res.current.temp_f;
 			});
 		});
 	}
@@ -55,8 +68,23 @@ document.getElementById("search_button").onclick =function(){
 		document.getElementById("precipitation").innerHTML= res.current.precip_mm+'mm';
 		document.getElementById("humidity").innerHTML = res.current.humidity;
 		document.getElementById("temp_logo").src = 'https:'+res.current.condition.icon;
+
+		celsius=    res.current.temp_c;
+		fahrenheit= res.current.temp_f;
 	});
 };
+
+
+//to change the measure of temperature
+document.getElementById("celsius_button").onclick= function(){
+	document.getElementById("fahrenheit_button").style.color = "blue";
+	document.querySelector(".temp_degree").innerHTML = celsius;
+}
+
+document.getElementById("fahrenheit_button").onclick= function(){
+	document.getElementById("celsius_button").style.color = "blue";
+	document.querySelector(".temp_degree").innerHTML = fahrenheit;
+}
 
 //the godly async fetch fuction.
 async function get_data(api){
